91b5d475a6695b033631ceaacbdde7f2748cd170
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 17:53:09 +0000

    modified the name

--- Hello.py
+++ Hello.py
@@ -18,7 +18,7 @@ COHERE_API_KEY =  "7Sc4f917kCqYSB3hyAYbsaCFLbFXZQUBucGKZjsw"
 DB_FAISS_PATH = r"vectorstore/db_faiss"
 embeddings = CohereEmbeddings(model="embed-english-light-v3.0")
 
-st.title("Stacy AI")
+st.title("Stacy S Nalin AI")
 
 db = FAISS.load_local(DB_FAISS_PATH, embeddings,allow_dangerous_deserialization=True)
 llm = ChatCohere(model="command-r")
