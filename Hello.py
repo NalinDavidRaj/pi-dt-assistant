c605a3e501e22980c1f06c0ff4b72dc7dc7c8ccb
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 11:44:29 +0000

    updated

--- Hello.py
+++ Hello.py
@@ -11,13 +11,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders.pdf import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.vectorstores import pinecone
+from langchain_cohere import ChatCohere
+from langchain_cohere import CohereEmbeddings
 import streamlit as st
 from streamlit.logger import get_logger
 import random
 import time
+import os
+import sys
+import getpass
 
 LOGGER = get_logger(__name__)
+os.environ["COHERE_API_KEY"] = getpass.getpass()
+DB_FAISS_PATH = "vectorstore/db_faiss"
+
+#Train with CSV files
+def train_model_With_CSV():
+    loader = CSVLoader(file_path="Data/sample.csv",encoding="utf8",csv_args={'delimiter':','})
+    data=loader.load()
+    #spilt the text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(data)
+    #Downlaod the model for embedding
+    embeddings = CohereEmbeddings(model="embed-english-light-v3.0")
+    #convert the text chunks into embedding and save into FAISS knowledge base
+    docsearch  = FAISS.from_documents(text_chunks,embeddings)
+    #save to vector Db
+    docsearch.save_local(DB_FAISS_PATH)
+
+   
 
 # Streamed response emulator
 def response_generator():
@@ -57,9 +88,8 @@ def chat():
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
-
-
-
-
+if st.button('Check availability'):
+   st.write('Why hello there')
+   
 if __name__ == "__main__":
     chat()
