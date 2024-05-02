3b60716b1ae8522cacaa86a8d44995af56a2fc09
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 18:03:03 +0000

    Commited

--- pidt.py
+++ pidt.py
@@ -1,61 +1,61 @@
-
-import streamlit as st
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders.pdf import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import CTransformers
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.vectorstores import pinecone
 from langchain_cohere import ChatCohere
 from langchain_cohere import CohereEmbeddings
-from streamlit.logger import get_logger
-
-
-COHERE_API_KEY =  "7Sc4f917kCqYSB3hyAYbsaCFLbFXZQUBucGKZjsw"
-DB_FAISS_PATH = r"vectorstore/db_faiss"
+import streamlit as st
+import os
+import sys
+import getpass
+ 
+os.environ["COHERE_API_KEY"] = "7Sc4f917kCqYSB3hyAYbsaCFLbFXZQUBucGKZjsw"
+#getpass.getpass()
+ 
+DB_FAISS_PATH = "vectorstore/db_faiss"
+# Read CSV files
+#loader = CSVLoader(file_path="Data/california_tour_package.csv", encoding="utf8", csv_args={'delimiter': ','})
+#data = loader.load()
+# print(data)
+ 
+loader = CSVLoader(file_path="Data/sample.csv", encoding="utf8", csv_args={'delimiter': ','})
+data = loader.load()
+ 
+#loader = PyPDFLoader("Data/Optum.pdf")
+#data = loader.load_and_split()
+ 
+# spilt the text into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+text_chunks = text_splitter.split_documents(data)
+# print(len(text_chunks))
+ 
+# download sentence trasnformers from ebedding from hugging face
+# embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 embeddings = CohereEmbeddings(model="embed-english-light-v3.0")
-
-st.title("ChatGPT-like clone")
-
-db = FAISS.load_local(DB_FAISS_PATH, embeddings,allow_dangerous_deserialization=True)
+ 
+# convert the text chunks into embedding and save into FAISS knowledge base
+docsearch = FAISS.from_documents(text_chunks, embeddings)
+ 
+docsearch.save_local(DB_FAISS_PATH)
+ 
+# calling llm model
 llm = ChatCohere(model="command-r")
-qa = ConversationalRetrievalChain.from_llm(llm,retriever=db.as_retriever())
-
-# Set OpenAI API key from Streamlit secrets
-#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-# Set a default model
-#if "coher_model" not in st.session_state:
-    #st.session_state["coher_model"] = "command-r"
-
-
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-        result = qa({"question":messages,"chat_history":messages})
-        response = st.write_stream(result['answer'])
-    st.session_state.messages.append({"role": "assistant", "content": response})
+qa = ConversationalRetrievalChain.from_llm(llm, retriever=docsearch.as_retriever())
+ 
+while True:
+    chat_history = []
+    query = input(f"Please enter your question : ")
+    if query == 'exit':
+        print("Exiting")
+        sys.exit()
+    if query == '':
+        continue
+    print("processing")
+    result = qa({"question": query, "chat_history": chat_history})
+    print("Response:", result['answer'])
