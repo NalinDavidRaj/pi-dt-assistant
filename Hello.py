e4a743214ba8f68048c986969b71c8653a758a9b
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 17:45:12 +0000

    Updated

--- Hello.py
+++ Hello.py
@@ -39,8 +39,8 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     query = prompt  # Use the user's input as the query
-    chat_history = [m["content"] for m in st.session_state.messages if m["role"] == "user"]
-    #chat_history=[]
+    #chat_history = [m["content"] for m in st.session_state.messages if m["role"] == "user"]
+    chat_history=[]
     result = qa({"question":query,"chat_history":chat_history})
     response = result["answer"]
 
