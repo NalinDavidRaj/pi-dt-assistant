14591b5adfb03df184e41f8c1464ae8247d6790c
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 12:12:25 +0000

    modified

--- Hello.py
+++ Hello.py
@@ -87,11 +87,9 @@ def chat():
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
-st.button("Reset", type="primary")
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+
+if st.button('Train'):
+    st.success('Training Successful!', icon="✅")
 
 #if __name__ == "__main__":
     #chat()
