e3cfcea9bbcf9c3d099993bd9a3b49fed1a46178
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 17:42:27 +0000

    updated

--- Hello.py
+++ Hello.py
@@ -50,4 +50,4 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         st.write(response)
 
-    #st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
