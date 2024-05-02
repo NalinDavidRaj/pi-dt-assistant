70bcd33e23fcb8fb891d9381cd34608ba97a29e4
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 11:14:51 +0000

    testing

--- Hello.py
+++ Hello.py
@@ -20,8 +20,20 @@ import time
 LOGGER = get_logger(__name__)
 
 
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
-def run():
+def chat():
     title = "Performance Insights - DT Assistant"
     st.set_page_config(
         page_title=title,
@@ -54,18 +66,7 @@ def run():
 
 
 
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+
 
 if __name__ == "__main__":
-    run()
+    chat()
