9fc7f303cd740ef3df7bc94c36a29c3f2c41f684
NalinDavidRaj <168332922+NalinDavidRaj@users.noreply.github.com>
2024-05-02 11:16:13 +0000

    change

--- Hello.py
+++ Hello.py
@@ -22,13 +22,7 @@ LOGGER = get_logger(__name__)
 
 # Streamed response emulator
 def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
+    response ="How can I assist you today?"
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
